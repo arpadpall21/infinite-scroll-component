8e6c9f37f5900a4b97f080083f23175f5b83184e
arpadpall21 <a.pall21@yahoo.fr>
2025-05-20 20:40:13 +0200

    initial working logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import InfiniteScroll from './InfiniteScroll/InfiniteScroll';
+
 const App = () => {
-  return <p> Hello World! </p>;
+  return (
+    <div style={{ margin: '100px auto' }}>
+      <InfiniteScroll />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/InfiniteScroll/InfiniteScroll.tsx
@@ -0,0 +1,97 @@
+import React, { useState, useEffect } from 'react';
+
+const cellWidth = 200;
+
+const InfiniteScroll = () => {
+  const [childIds, setChildIds] = useState<number[]>([0, 1, 2, 3, 4, 5, 6, 7]);
+
+  const [position, setPosition] = useState({ x: 0 });
+  const [isDragging, setIsDragging] = useState(false);
+  const [offset, setOffset] = useState({ x: 0 });
+  const [gridOffset, setGridOffset] = useState<number>(0);
+
+  useEffect(() => {
+    console.log('--- use effect ---')
+    console.log(gridOffset)
+  }, [gridOffset])
+
+
+  function handleMouseMove(e: React.MouseEvent) {
+    if (isDragging) {
+      setPosition({ x: e.clientX - offset.x });
+      
+      // handle right slide
+      if (position.x > gridOffset + cellWidth) {
+        setGridOffset(gridOffset + cellWidth);
+        
+        
+        const childIdsClone = [...childIds]
+        const originalLength = childIdsClone.length;
+        childIdsClone.unshift(childIdsClone[0] - 1);
+        setChildIds(childIdsClone.slice(0, originalLength));
+      } else if (position.x < gridOffset) {
+        setGridOffset(gridOffset - cellWidth);
+        
+        const childIdsClone = [...childIds]
+        childIdsClone.push(childIdsClone[childIdsClone.length -1] + 1);
+        childIdsClone.shift()
+        setChildIds(childIdsClone);
+      }
+    }
+  }
+
+
+
+  function handleMouseDown(e: React.MouseEvent) {
+    e.preventDefault();
+    setIsDragging(true);
+    setOffset({ x: e.clientX - position.x  });
+  }
+
+  return (
+    <div 
+      style={{
+        position: 'relative',
+        border: 'solid 3px black',
+        height: 150,
+        width: 1300,
+        margin: 'auto',
+        overflow: 'hidden'
+      }}
+      onMouseLeave={() => {
+        console.log('--- mouse left ---')
+        setIsDragging(false)
+      }}
+      >
+      <div
+        style={{
+          position: 'absolute',
+          right: gridOffset,
+          border: 'solid 5px green',
+          textWrap: 'nowrap',
+          transform: `translate(${position.x}px`,
+          cursor: isDragging ? 'grabbing' : 'grab',
+        }}
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        onMouseUp={() => setIsDragging(false)}
+      >
+        {childIds.map((id) => (
+          <div 
+            style={{
+              display: 'inline-block',
+              border: 'solid 1px red',
+              height: 130,
+              width: cellWidth,
+              backgroundColor: 'orange',
+              opacity: 0.9,
+            }}
+            key={id}>{id}
+          </div>)
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default InfiniteScroll;
